36172c9a427254f3bbf14855f460449320c0461d
Adelakin Israel <adelakinisrael024@gmail.com>
2022-10-29 19:40:29 +0100

    updated

--- hngApp/urls.py
+++ hngApp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('',views.api_response),
+    path('',views.home),
 ]
--- hngApp/views.py
+++ hngApp/views.py
@@ -3,6 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
 
-def api_response(request, *args, **kwargs):
-    return JsonResponse({"slackUsername":"Ezrahel","Backend": True,"Age":24,"Bio":"Am a backend engineer, am comfortable with python,django HTML&CSS. Fun fact: Am a Tech enthusiast and a table tennis geek"})
+
+@api_view(["GET"])
+def home(request, *args, **kwargs):
+  header = {"Access-Control-Allow-Origin":"*"}
+  data = {
+    "slackUsername":"Ez'rahel",
+    "backend":True,
+    "age": 24,
+    "bio":"Am a backend engineer, am comfortable with python django"
+  }
+  return JsonResponse(data,headers=header)
